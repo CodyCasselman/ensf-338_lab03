352eedfce24b88bb331aa6135d22b08025306baf
Keeryn <keerynjohnson@gmail.com>
2024-02-06 17:44:30 -0700

    ex4

---
+++ ex4.py
@@ -0,0 +1,42 @@
+import timeit
+import random
+import matplotlib.pyplot as plt
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+def generate_worst_case_input(size):
+    return list(range(size, 0, -1))
+
+# Measure execution time using timeit.default_timer()
+def measure_execution_time(func, *args):
+    start_time = timeit.default_timer()
+    result = func(*args)
+    end_time = timeit.default_timer()
+    execution_time = end_time - start_time
+    return result, execution_time
+
+# Part 3: Run quicksort on inputs of increasing sizes
+input_sizes = list(range(10, 510, 10))
+execution_times = []
+
+for size in input_sizes:
+    input_data = generate_worst_case_input(size)
+    _, execution_time = measure_execution_time(quicksort, input_data)
+    execution_times.append(execution_time)
+
+# Part 4: Plot the results
+plt.plot(input_sizes, execution_times, linestyle='-', label='Quicksort')
+plt.xlabel('Input Size')
+plt.ylabel('Execution Time (seconds)')
+plt.title('Quicksort Execution Time on Worst-case Inputs')
+plt.legend()
+plt.show()
+
+
